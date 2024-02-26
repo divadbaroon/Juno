ddf73fcb110d19403b927023cc2ed1a0ba0e3897
David <dbarron410@vt.edu>
2024-02-26 15:20:28 -0500

    Add image remote patterns and update layout and page components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import { Collection } from "@/components/shared/Collection"
 import { navLinks } from "@/constants"
+import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -6,7 +8,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
 
-  const images = ""
+  const images = await getAllImages({ page, searchQuery})
 
   return (
     <>
@@ -31,7 +33,12 @@ const Home = async ({ searchParams }: SearchParamProps) => {
       </section>
 
       <section className="sm:mt-12">
-        
+        <Collection 
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
       </section>
     </>
   )
--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -2,9 +2,9 @@ import { auth } from "@clerk/nextjs";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
-// import { Collection } from "@/components/shared/Collection";
-// import Header from "@/components/shared/Header";
-// import { getUserImages } from "@/lib/actions/image.actions";
+import { Collection } from "@/components/shared/Collection";
+import Header from "@/components/shared/Header";
+import { getUserImages } from "@/lib/actions/image.actions";
 import { getUserById } from "@/lib/actions/user.actions";
 
 const Profile = async ({ searchParams }: SearchParamProps) => {
@@ -14,9 +14,11 @@ const Profile = async ({ searchParams }: SearchParamProps) => {
   if (!userId) redirect("/sign-in");
 
   const user = await getUserById(userId);
+  const images = await getUserImages({ page, userId: user._id });
 
   return (
     <>
+      <Header title="Profile" />
 
       <section className="profile">
         <div className="profile-balance">
@@ -43,13 +45,17 @@ const Profile = async ({ searchParams }: SearchParamProps) => {
               height={50}
               className="size-9 md:size-12"
             />
-            <h2 className="h2-bold text-dark-600">{}</h2>
+            <h2 className="h2-bold text-dark-600">{images?.data.length}</h2>
           </div>
         </div>
       </section>
 
       <section className="mt-8 md:mt-14">
-        
+        <Collection
+          images={images?.data}
+          totalPages={images?.totalPages}
+          page={page}
+        />
       </section>
     </>
   );
