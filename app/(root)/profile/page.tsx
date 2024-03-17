302cab158a5db8cce00c04ec603c20112a72d1be
David <dbarron410@vt.edu>
2024-03-17 02:17:15 -0400

    Remove unused code and files

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -4,7 +4,6 @@ import { redirect } from "next/navigation";
 
 import { Collection } from "@/components/shared/Collection";
 import Header from "@/components/shared/Header";
-import { getUserImages } from "@/lib/actions/image.actions";
 import { getUserById } from "@/lib/actions/user.actions";
 
 const Profile = async ({ searchParams }: SearchParamProps) => {
@@ -14,7 +13,6 @@ const Profile = async ({ searchParams }: SearchParamProps) => {
   if (!userId) redirect("/sign-in");
 
   const user = await getUserById(userId);
-  const images = await getUserImages({ page, userId: user._id });
 
   return (
     <>
@@ -45,19 +43,11 @@ const Profile = async ({ searchParams }: SearchParamProps) => {
               height={50}
               className="size-9 md:size-12"
             />
-            <h2 className="h2-bold text-dark-600">{images?.data.length}</h2>
           </div>
         </div>
       </section>
 
-      <section className="mt-8 md:mt-14">
-        <Collection
-          images={images?.data}
-          totalPages={images?.totalPages}
-          page={page}
-          libraryType={'profile'}
-        />
-      </section>
+      
     </>
   );
 };
