82fc79b4d792f5e3ace357ff66085b226c66911f
David <dbarron410@vt.edu>
2024-03-20 15:22:31 -0400

    Update welcome message in page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,7 +11,7 @@ const Home = () => {
         <div className="space-y-6">
           <h4 className="h2-bold text-dark-600" style={{ fontSize: '55px', marginTop: '0px' }}>Welcome to Juno</h4>
           <p className="p-20-regular text-dark-400" style={{ marginTop: '15px' }}>
-            Customize, create, and seamlessly interact with your own personalized AI.
+            The all-in-one platform for steamlined AI customization and seamless interaction.
           </p>
           
         </div>
