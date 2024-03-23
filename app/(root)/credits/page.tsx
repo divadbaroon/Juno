ed95baba93c43047fb686e710cb11e1be0075572
David <dbarron410@vt.edu>
2024-03-22 21:06:54 -0400

    Refactor credits page layout and display usage in hours or minutes

--- app/(root)/credits/page.tsx
+++ app/(root)/credits/page.tsx
@@ -28,11 +28,11 @@ const Credits = async () => {
             <li key={plan.name} className="credits-item">
               <div className="flex-center flex-col gap-3">
                 <Image src={plan.icon} alt="check" width={50} height={50} />
-                <p className="p-20-semibold mt-2 text-purple-500">
-                  {plan.name}
-                </p>
+                <p className="p-20-semibold mt-2 text-purple-500">{plan.name}</p>
                 <p className="h1-semibold text-dark-600">${plan.price}</p>
-                <p className="p-16-regular">Usage/day: {plan.usage}</p>
+                <p className="p-16-regular">
+                  Usage/day: {plan.usage} {plan.price === 0 ? 'minutes' : 'hours'}
+                </p>
               </div>
 
               {/* Inclusions */}
