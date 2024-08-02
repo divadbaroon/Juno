52a96dd5192a29fcfd777db19fce261337b450f2
David <dbarron410@vt.edu>
2024-08-01 21:19:55 -0700

    change color of cleark appearance

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider appearance={{
-      variables: { colorPrimary: '#624cf5' }
+      variables: { colorPrimary: '#1a1a1a' }
     }}>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
