2ee56b5521efc4dc5dfeab071555fdcbfd5d4ccb
David <dbarron410@vt.edu>
2024-08-03 17:28:32 -0700

    refactor profiles webhook

--- app/api/webhooks/profiles/route.ts
+++ app/api/webhooks/profiles/route.ts
@@ -1,21 +1,12 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { getAllProfiles } from '../../../../lib/actions/fetchProfileData.action'
-import { IProfile } from '../../../../lib/database/models/profile.model' 
+import { NextRequest, NextResponse } from 'next/server'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<IProfile[] | { error: string }>
-) {
-  if (req.method === 'GET') {
-    try {
-      const profiles = await getAllProfiles()
-      res.status(200).json(profiles)
-    } catch (error) {
-      console.error('Failed to retrieve Profile documents:', error)
-      res.status(500).json({ error: 'Internal Server Error' })
-    }
-  } else {
-    res.setHeader('Allow', ['GET'])
-    res.status(405).end(`Method ${req.method} Not Allowed`)
+export async function GET(request: NextRequest) {
+  try {
+    const profiles = await getAllProfiles()
+    return NextResponse.json(profiles)
+  } catch (error) {
+    console.error('Failed to retrieve Profile documents:', error)
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
   }
 }
