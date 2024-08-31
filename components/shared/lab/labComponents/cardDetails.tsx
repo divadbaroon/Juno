2c1de9cc4dd0198b0f24a9fcfb9f1cbf840e40ed
David <dbarron410@vt.edu>
2024-08-31 15:19:34 -0700

    Got rid of outline around input field

--- components/shared/lab/labComponents/cardDetails.tsx
+++ components/shared/lab/labComponents/cardDetails.tsx
@@ -93,6 +93,7 @@ const CardDetails: React.FC<CardDetailsProps> = ({
                 <FormDescription style={{ marginTop: '.1rem'}}>{fieldName}</FormDescription>
                 <FormControl>
                   <Input
+                    className="focus-visible:ring-0"
                     {...field}
                     onChange={(e) => {
                       field.onChange(e);
@@ -115,7 +116,7 @@ const CardDetails: React.FC<CardDetailsProps> = ({
                 <FormControl>
                   <textarea
                     {...field}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300 sm:text-sm sm:leading-6"
                     onChange={(e) => {
                       field.onChange(e);
                       setDescription(e.target.value);
@@ -182,11 +183,11 @@ const CardDetails: React.FC<CardDetailsProps> = ({
             render={() => (
               <FormItem>
                 <FormLabel className="font-bold" style={{ color: '#373737' }}>Tags</FormLabel>
-                <FormDescription style={{ marginTop: '.1rem' }}>
+                <FormDescription style={{ marginTop: '.1rem', marginBottom: '1rem' }}>
                   Add tags to help users find your {cardType}.
                 </FormDescription>
                 <FormControl>
-                  <div className="mt-2">
+                  <div className="-ml-2">
                     <Filter 
                     contextType={contextType} cardType={cardType}
                     onTagsChange={setTags}
