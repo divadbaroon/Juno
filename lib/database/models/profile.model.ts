b1850897234e43da53563365eb3e7a6b6cdc3eba
David <dbarron410@vt.edu>
2024-08-30 19:42:17 -0700

    refactor user profile to include a reference (string ID) to a prompt object rather than store its values directly within the profile

--- lib/database/models/profile.model.ts
+++ lib/database/models/profile.model.ts
@@ -2,38 +2,34 @@ import { Document, Schema, model, models } from "mongoose";
 
 export interface IProfile extends Document {
   creator: string;
+  llm: string;
   name: string;
   description: string;
-  llm: string;
-  personality: string;
-  identity: string;
-  interactionGuidelines: string;
   voice: string;
   extensions: string[];
   sharePreference: string;
   photo?: string;
-  context: string;
-  temperature: number;
+  prompt: string;
+  tags: string[];
   createdAt?: Date;
   updatedAt?: Date;
+  index: number;
 }
 
 const ProfileSchema = new Schema({
   creator: { type: String, required: true },
+  llm: { type: String, required: true },
   name: { type: String, required: true },
   description: { type: String, required: true },
-  llm: { type: String, required: true },
-  personality: { type: String, required: true },
-  identity: { type: String, required: true },
-  interactionGuidelines: { type: String, required: true },
   voice: { type: String, required: true },
   extensions: [{ type: String }],
   sharePreference: { type: String, required: true },
+  prompt: { type: String, required: true },
   photo: { type: String },
-  context: { type: String, required: true },
-  temperature: { type: Number, required: true },
+  tags: { type: [String], required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
+  index: { type: Number, required: true },
 });
 
 const Profile = models?.Profile || model('Profile', ProfileSchema);
