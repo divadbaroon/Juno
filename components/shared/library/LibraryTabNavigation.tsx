d966a737ba4a18eb5b10e3cd7745c37082114876
David <dbarron410@vt.edu>
2024-09-09 02:17:18 -0700

    Made vertical bars taller

--- components/shared/library/LibraryTabNavigation.tsx
+++ components/shared/library/LibraryTabNavigation.tsx
@@ -3,18 +3,18 @@ import { Separator } from "@/components/ui/separator";
 
 const LibraryTabNavigation = () => {
     return (
-    <div className="flex h-5 items-center space-x-4 text-sm">
-        <Separator orientation="vertical" />
+    <div className="flex h-2 items-center space-x-4 text-sm">
+        <Separator orientation="vertical" className="h-6" />
         <Link href="/library/profiles" className="p-20-regular text-dark-400 cursor-pointer">Profiles</Link>
-        <Separator orientation="vertical" />
+        <Separator orientation="vertical" className="h-6" />
         <Link href="/library/llms" className="p-20-regular text-dark-400 cursor-pointer">LLMs</Link>
-        <Separator orientation="vertical" />
+        <Separator orientation="vertical" className="h-6" />
         <Link href="/library/prompts" className="p-20-regular text-dark-400 cursor-pointer">Prompts</Link>
-        <Separator orientation="vertical" />
+        <Separator orientation="vertical" className="h-6" />
         <Link href="/library/voices" className="p-20-regular text-dark-400 cursor-pointer">Voices</Link>
-        <Separator orientation="vertical" />
+        <Separator orientation="vertical" className="h-6" />
         <Link href="/library/extensions" className="p-20-regular text-dark-400 cursor-pointer">Extensions</Link>
-        <Separator orientation="vertical" />
+        <Separator orientation="vertical" className="h-6" />
     </div>
     )
 }
