9dea9551afc235ec98f9d1e4560ab7053438dcdd
David <dbarron410@vt.edu>
2024-09-09 02:16:43 -0700

    Made vertical bars taller

--- components/shared/lab/LabTabNavigation.tsx
+++ components/shared/lab/LabTabNavigation.tsx
@@ -3,16 +3,16 @@ import { Separator } from "@/components/ui/separator";
 
 const LabTabNavigation = () => {
     return (
-    <div className="flex h-5 items-center space-x-4 text-sm">
-        <Separator orientation="vertical" />
+    <div className="flex h-2 items-center space-x-4 text-sm">
+        <Separator orientation="vertical" className="h-6" />
         <Link href="/lab/profile-creation" className="p-20-regular text-dark-400 cursor-pointer">Profile Creation</Link>
-        <Separator orientation="vertical" />
+        <Separator orientation="vertical" className="h-6" />
         <Link href="/lab/prompt-creation" className="p-20-regular text-dark-400 cursor-pointer">Prompt Creation</Link>
-        <Separator orientation="vertical" />
+        <Separator orientation="vertical" className="h-6" />
         <Link href="/lab/voice-cloning" className="p-20-regular text-dark-400 cursor-pointer">Voice Cloning</Link>
-        <Separator orientation="vertical" />
+        <Separator orientation="vertical" className="h-6" />
         <Link href="/lab/extension-creation" className="p-20-regular text-dark-400 cursor-pointer">Extension Creation</Link>
-        <Separator orientation="vertical" />
+        <Separator orientation="vertical" className="h-6" />
     </div>
     )
 }
