1217cac66a59c5433cd547462c54a4a8c2759cbe
David <dbarron410@vt.edu>
2024-09-11 04:17:31 -0700

    Refactored the sidebar to made it look more modern

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,116 +1,118 @@
 "use client"
 
-import { navLinks } from '@/constants'
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { Zap, BookOpen, Beaker, BarChart2, HelpCircle, UploadCloud, User, ChevronLeft, ChevronRight, Menu } from 'lucide-react'
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { Button } from '../ui/button'
-
 import { Separator } from "@/components/ui/separator"
 
+const mainMenuItems = [
+  { icon: Zap, label: 'Quick Start', route: '/quickstart' },
+  { icon: BookOpen, label: 'Library', route: '/library/profiles' },
+  { icon: Beaker, label: 'Lab', route: '/lab/profile-creation' },
+  { icon: BarChart2, label: 'Dashboard', route: '/dashboard' },
+]
+
+const bottomMenuItems = [
+  { icon: UploadCloud, label: 'Upgrade', route: '/upgrade' },
+]
 
 const Sidebar = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(true)
 
-  const getIconPath = (label: string) => {
-    switch (label) {
-      case 'Quick Start':
-        return 'm3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z';
-      case 'Lab':
-        return 'M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5';
-      case 'Library':
-        return 'M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z';
-      case 'Dashboard':
-        return 'M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z';
-      case 'Guide':
-        return 'M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z';
-      case 'Upgrade':
-        return 'M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z';
-      default:
-        return '';
-    }
-  };
+  const renderMenuItem = (item: any, index: any) => (
+    <li key={index}>
+      <Button
+        asChild
+        variant="ghost"
+        className={cn(
+          "flex items-center w-full px-4 py-3 text-left transition-colors group",
+          item.route === pathname 
+            ? "bg-purple-gradient text-white hover:bg-purple-gradient hover:text-white" 
+            : "hover:bg-gray-200 text-gray-700",
+          "justify-start"
+        )}
+      >
+        <Link href={item.route} className="flex items-center w-full">
+          <item.icon className={cn(
+            "flex-shrink-0 w-6 h-6 mr-3",
+            item.route === pathname 
+              ? "text-white group-hover:text-white" 
+              : "text-gray-700 group-hover:text-gray-900"
+          )} />
+          <span className={cn(
+            item.route === pathname 
+              ? "text-white group-hover:text-white" 
+              : "text-gray-700 group-hover:text-gray-900"
+          )}>
+            {item.label}
+          </span>
+        </Link>
+      </Button>
+    </li>
+  )
 
   return (
-    <aside className="sidebar">
-      <div className="flex size-full flex-col gap-4">
-        <Link
-          href="/"
-          style={{ marginTop: '-72px', marginBottom: '-104px', marginLeft: '-15px' }}
-          className="sidebar-logo"
-        >
-          <Image src="/assets/icons/junologo2.svg" alt="logo" width={300} height={300} />
-        </Link>
-        <Separator className="my-5" />
-        <nav className="sidebar-nav -mt-4">
-          <SignedIn>
-            <ul className="sidebar-nav_elements">
-              {navLinks.slice(0, 4).map((link) => {
-                const isActive = link.route === pathname;
-                const iconPath = getIconPath(link.label);
-                return (
-                  <li
-                    key={link.route}
-                    className={`sidebar-nav_element group ${
-                      isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
-                    }`}
-                  >
-                    <Link className="sidebar-link" href={link.route}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        className={`w-6 h-6 navbar-icon ${isActive ? 'navbar-icon-active' : ''}`}
-                      >
-                        <path strokeLinecap="round" strokeLinejoin="round" d={iconPath} />
-                      </svg>
-                      {link.label}
-                    </Link>
-                  </li>
-                );
-              })}
+    <>
+      <Button
+        variant="ghost"
+        size="sm"
+        className={cn(
+          "fixed top-4 left-4 p-2 z-50 transition-all duration-300 -ml-5",
+          isOpen ? "left-64" : "left-4"
+        )}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? <ChevronLeft size={24} /> : <Menu size={24} />}
+      </Button>
+      
+      <aside className={cn(
+        "sidebar fixed top-0 left-0 flex flex-col h-screen bg-gray-50 text-gray-700 transition-all duration-300 z-40",
+        isOpen ? "w-64" : "w-0 -left-64"
+      )}>
+        <div className="flex flex-col h-full overflow-y-auto">
+          <div className="p-4 flex justify-center items-center -mt-9">
+            <Link href="/" className="sidebar-logo">
+              <Image 
+                src="/assets/icons/junologo2.svg" 
+                alt="Juno logo" 
+                width={180}
+                height={80}
+                className="transition-all duration-300"
+              />
+            </Link>
+          </div>
+          <Separator className="my-2 -mt-12" />
+          <nav className="sidebar-nav flex-grow">
+            <SignedIn>
+              <ul className="space-y-2">
+                {mainMenuItems.map(renderMenuItem)}
+              </ul>
+            </SignedIn>
+          </nav>
+          <div className="mt-auto">
+            <ul className="space-y-2">
+              {bottomMenuItems.map(renderMenuItem)}
             </ul>
-            <ul className="sidebar-nav_elements">
-              {navLinks.slice(4).map((link) => {
-                const isActive = link.route === pathname;
-                const iconPath = getIconPath(link.label);
-                return (
-                  <li
-                    key={link.route}
-                    className={`sidebar-nav_element group ${
-                      isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
-                    }`}
-                  >
-                    <Link className="sidebar-link" href={link.route}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        className={`w-6 h-6 navbar-icon ${isActive ? 'navbar-icon-active' : ''}`}
-                      >
-                        <path strokeLinecap="round" strokeLinejoin="round" d={iconPath} />
-                      </svg>
-                      {link.label}
-                    </Link>
-                  </li>
-                );
-              })}
-              <li className="flex-center cursor-pointer gap-2 p-4">
+            <SignedIn>
+              <div className="mt-4 px-4 mb-3">
                 <UserButton afterSignOutUrl="/" showName />
-              </li>
-            </ul>
-          </SignedIn>
-          <SignedOut>
-            <Button asChild className="button bg-purple-gradient bg-cover">
-              <Link href="/sign-in">Login</Link>
-            </Button>
-          </SignedOut>
-        </nav>
-      </div>
-    </aside>
+              </div>
+            </SignedIn>
+            <SignedOut>
+              <Button asChild className="button bg-purple-gradient bg-cover w-full mt-4">
+                <Link href="/sign-in">Login</Link>
+              </Button>
+            </SignedOut>
+          </div>
+        </div>
+      </aside>
+    </>
   );
 };
 
